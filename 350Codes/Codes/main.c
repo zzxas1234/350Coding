e974fc776de5ff3345cde69482e3f2c59d4c3338
Ezilon <zzxas1234@hotmail.co.th>
2019-09-17 19:14:54 +1000

    .

--- 350Codes/Codes/main.c
+++ 350Codes/Codes/main.c
@@ -1,27 +1,22 @@
-/* ADC1 Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "driver/adc.h"
+//#include "driver/dac.h"
 #include "esp_adc_cal.h"
+#include "time.h"
 
 #define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
 #define NO_OF_SAMPLES   64          //Multisampling
 
 static esp_adc_cal_characteristics_t *adc_chars;
 
-static const adc_channel_t fsr1Channel = ADC1_CHANNEL_4;    //Pin32
-static const adc_channel_t fsr2Channel = ADC1_CHANNEL_5;    //Pin33
-static const adc_channel_t utsChannel = ADC1_CHANNEL_6;     //PinA2
+static const adc_channel_t fsr1Channel = ADC1_CHANNEL_4;            //Pin32
+static const adc_channel_t fsr2Channel = ADC1_CHANNEL_5;            //Pin33
+static const adc_channel_t utsChannel = ADC1_CHANNEL_6;             //PinA2
+static const adc_channel_t calibrationChannel = ADC1_CHANNEL_3;     //PinA3
 
 static const adc_atten_t atten = ADC_ATTEN_DB_0;
 static const adc_unit_t unit = ADC_UNIT_1;
@@ -29,6 +24,16 @@ static const adc_unit_t unit = ADC_UNIT_1;
 static int fsr1Value;
 static int fsr2Value;
 static int utsValue;
+
+static  int fsr1MaxValue;
+static  int fsr2MaxValue;
+static  int utsMaxValue;
+
+static  int currentFsr1Value;
+static  int currentFsr2Value;
+static  int currentUtsValue;
+
+static  bool calibrationbotton;
 
 static void systemInitialise(void)
 {
@@ -97,15 +102,102 @@ static void fsr1Read(void)
 }
 static void fsr2Read(void)
     {
-
+        uint32_t fsr2_reading = 0;
+        //Multisampling
+    for (int i = 0; i < NO_OF_SAMPLES; i++) 
+    {
+        if (unit == ADC_UNIT_1) {
+            fsr2_reading += adc1_get_raw((adc1_channel_t)fsr2Channel);
+        } 
+        else 
+        {
+            int raw;
+            adc2_get_raw((adc2_channel_t)fsr2Channel, ADC_WIDTH_BIT_12, &raw);
+            fsr2_reading += raw;
+        }
     }
+    fsr2_reading /= NO_OF_SAMPLES;
+    //Convert adc_reading to voltage in mV
+    uint32_t voltage = esp_adc_cal_raw_to_voltage(fsr2_reading, adc_chars);
+    printf("FSR2 Reading --> Raw: %d\tVoltage: %dmV\n", fsr2_reading, voltage);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    fsr2Value = (int)fsr2_reading;
+    }
+
 static void utsRead(void)
     {
+        uint32_t uts_reading = 0;
+        //Multisampling
+    for (int i = 0; i < NO_OF_SAMPLES; i++) 
+    {
+        if (unit == ADC_UNIT_1) {
+            uts_reading += adc1_get_raw((adc1_channel_t)utsChannel);
+        } 
+        else 
+        {
+            int raw;
+            adc2_get_raw((adc2_channel_t)utsChannel, ADC_WIDTH_BIT_12, &raw);
+            uts_reading += raw;
+        }
+    }
+    uts_reading /= NO_OF_SAMPLES;
+    //Convert adc_reading to voltage in mV
+    uint32_t voltage = esp_adc_cal_raw_to_voltage(uts_reading, adc_chars);
+    printf("FSR1 Reading --> Raw: %d\tVoltage: %dmV\n", uts_reading, voltage);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    utsValue = (int)uts_reading;
+    }
+
+static void calibration(void)
+    {
+    calibrationStart = true;
+    fsr1MaxValue = ((fsr1Value >= fsr1MaxValue) == 1)? fsr1Value:fsr1MaxValue;
+    fsr2MaxValue = ((fsr2Value >= fsr2MaxValue) == 1)? fsr2Value:fsr2MaxValue;
 
     }
+//usinng adc1 for switch>>A1
+static int calibrationSwitch(int modes)
+{
+    bool bottonPressed;
+    int holdingTime = 5;
+    clock_t start = clock();  
+    //botton realease > measure time > if more than 5 go to next one
+    if (modes == 0)
+    {
+        clock_t stop = clock();
+        double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+        printf("Time elapsed in ms: %f", elapsed);
+        if ((int)elapsed >= holdingTime)
+        {
+            while(~bottonPressed)
+            {
+                calibration();
+            }
+        
+        }
+    }
+    //input = 0
+    else if (modes == 1)
+    {
+    }
+}
+
+static void calibrationBottonInputCapture(void)
+{
+//if calibration botton is pressed>=1 if not = 0
+}
+
 
 void app_main(void)
+{
+        //Check if Two Point or Vref are burned into eFuse
+    check_efuse();
+    while(1)
     {
-    
+        fsr1Read();
+        fsr2Read();
+        utsRead();
     }
+    
+}
 
